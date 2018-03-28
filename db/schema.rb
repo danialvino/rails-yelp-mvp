2efa7e4e92afb3b1a2a37df27af4887779173fc5
Danielle Alvino <daniellealvino@hotmail.com>
2018-03-28 11:25:27 -0300

    generate review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328142214) do
+ActiveRecord::Schema.define(version: 20180328142443) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20180328142214) do
     t.integer "phone"
     t.text "category"
     t.text "reviews"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
